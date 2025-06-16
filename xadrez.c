a4878db6d0b316fbf8f2505c532f8bb8b04950a4
pnevesp <164400676+pnevesp@users.noreply.github.com>
2025-06-16 18:36:01 +0000

    Código com for, while e do while corrigido

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    // Movimento da Torre: 5 casas para a direita (usando for)
     printf("Movimento da Torre:\n");
     for (int i = 1; i <= 5; i++) {
         printf("Direita\n");
     }
 
-    // Movimento do Bispo: 5 casas na diagonal (cima e direita) (usando while)
     printf("\nMovimento do Bispo:\n");
     int j = 1;
     while (j <= 5) {
@@ -15,7 +13,6 @@ int main() {
         j++;
     }
 
-    // Movimento da Rainha: 8 casas para a esquerda (usando do-while)
     printf("\nMovimento da Rainha:\n");
     int k = 1;
     do {
